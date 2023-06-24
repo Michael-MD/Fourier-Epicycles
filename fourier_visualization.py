4be27ed484ada3202ad750ff61798ce93545d476
Michael-MD <mdei0001@student.monash.edu>
2023-06-24 15:46:06 +1000

    only plots largest curve

--- fourier_visualization.py
+++ fourier_visualization.py
@@ -9,6 +9,9 @@ from tqdm import tqdm
 
 plt.style.use('dark_background')
 
+img = sys.argv[1]
+N_desired = int(sys.argv[2]) if sys.argv[2] != 'None' else None
+export_anim = True if sys.argv[3] == '1' else False
 
 def prepare_image(img, N_desired=300):
 	img_gray = np.average(
@@ -43,8 +46,7 @@ def prepare_image(img, N_desired=300):
 		ind = np.argmin(
 				d:=np.abs(p - coords)
 			)
-		print(d[ind])
-		if d[ind] > 1.5:
+		if d[ind] > 2:
 			# jumped to different curve
 			cycles.append(coords_ordered)
 			coords_ordered = []
@@ -58,78 +60,74 @@ def prepare_image(img, N_desired=300):
 
 	skip_factor = np.max([int(N / N_desired),1])
 	for i,cycle in enumerate(cycles):
-		cycles[i] = cycle[0::skip_factor]
+		cycles[i] = np.asarray(cycle[0::skip_factor])
 
-	for cycle in cycles:
-		plt.plot(np.real(cycle), np.imag(cycle))
-		plt.show()
+	ind = np.argmax(map(len, cycles))
 
+	return cycles[ind]
 
-	return cycles
+coords = prepare_image(img, N_desired)
+N = len(coords)
 
+# treat points as complex function and find contained frequencies
+f = np.fft.fftshift(np.fft.fftfreq(N))
+Xf = np.fft.fftshift(np.fft.fft(coords))
 
-coords = prepare_image(sys.argv[1], int(sys.argv[2]) if sys.argv[2] != 'None' else None )
-# N = len(coords)
-
-# # treat points as complex function and find contained frequencies
-# f = np.fft.fftshift(np.fft.fftfreq(N))
-# Xf = np.fft.fftshift(np.fft.fft(coords))
-
-# x = lambda n: np.exp(1j*2*np.pi*f*n)*Xf/N
-# xn = [x(n) for n in range(N)]
-# x_sum = [np.sum(xni) for xni in xn]
-# pn = [ np.insert(np.cumsum(xni), 0, 0+0j) for xni in xn]
-# thetan, rn = np.angle(pn), np.abs(pn)
-# rn_single = np.abs(xn)
+x = lambda n: np.exp(1j*2*np.pi*f*n)*Xf/N
+xn = [x(n) for n in range(N)]
+x_sum = [np.sum(xni) for xni in xn]
+pn = [ np.insert(np.cumsum(xni), 0, 0+0j) for xni in xn]
+thetan, rn = np.angle(pn), np.abs(pn)
+rn_single = np.abs(xn)
 
 
-# fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
-# ax.axis('off')
-# ax.set_ylim([np.pi*2,np.max(np.abs(coords))*1.1])
+fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+ax.axis('off')
+ax.set_ylim([np.pi*2,np.max(np.abs(coords))*1.1])
 
-# p_bar = tqdm(range(frames:=N*2))
+p_bar = tqdm(range(frames:=N*2))
 
-# class update_cls:
-# 	def __init__(self):
-# 		self.epicycles = []
-# 		self.epicyle_radii = None
+class update_cls:
+	def __init__(self):
+		self.epicycles = []
+		self.epicyle_radii = None
 
-# 	def draw_circle(self,c,r):
-# 		circle = Circle(c, r, transform=ax.transData._b, edgecolor='white', facecolor='none')
-# 		s = ax.add_artist(circle)
-# 		ax.set_aspect('equal')
-# 		self.epicycles.append(s)
+	def draw_circle(self,c,r):
+		circle = Circle(c, r, transform=ax.transData._b, edgecolor='white', facecolor='none')
+		s = ax.add_artist(circle)
+		ax.set_aspect('equal')
+		self.epicycles.append(s)
 
-# 	def __call__(self,n):
-# 		global s
-# 		# update progress bar
-# 		p_bar.n = n
-# 		p_bar.refresh()
+	def __call__(self,n):
+		global s
+		# update progress bar
+		p_bar.n = n
+		p_bar.refresh()
 
-# 		# draw nth point
-# 		if n < N:
-# 			ax.plot(np.angle(x_sum[n-1:n+1]), np.abs(x_sum[n-1:n+1]), 'w')
+		# draw nth point
+		if n < N:
+			ax.plot(np.angle(x_sum[n-1:n+1]), np.abs(x_sum[n-1:n+1]), 'w')
 
-# 		n %= N
-# 		p = pn[n]
+		n %= N
+		p = pn[n]
 
-# 		# remove previous epicycle drawings
-# 		if self.epicyle_radii is not None:
-# 			[epicycle.remove() for epicycle in self.epicycles]
-# 			[epicycle_radus.remove() for epicycle_radus in self.epicyle_radii]
-# 			self.epicycles = []
+		# remove previous epicycle drawings
+		if self.epicyle_radii is not None:
+			[epicycle.remove() for epicycle in self.epicycles]
+			[epicycle_radus.remove() for epicycle_radus in self.epicyle_radii]
+			self.epicycles = []
 
-# 		theta, r = thetan[n], rn[n]
-# 		self.epicyle_radii = ax.plot(theta, r, 'w')
+		theta, r = thetan[n], rn[n]
+		self.epicyle_radii = ax.plot(theta, r, 'w')
 
-# 		# draw epicycle circles
-# 		for ri, pi in zip(rn_single[n], p):
-# 			self.draw_circle([np.real(pi),np.imag(pi)], ri)
+		# draw epicycle circles
+		for ri, pi in zip(rn_single[n], p):
+			self.draw_circle([np.real(pi),np.imag(pi)], ri)
 
-# update = update_cls()
+update = update_cls()
 
-# anim = animation.FuncAnimation(fig, update, frames=frames, interval=1)
-# if sys.argv[3] == '1':
-# 	anim.save('animation.gif', writer='imagemagick', fps=60)
+anim = animation.FuncAnimation(fig, update, frames=frames, interval=1)
+if export_anim:
+	anim.save('animation.gif', writer='imagemagick', fps=60)
 
-# plt.show()
+plt.show()
